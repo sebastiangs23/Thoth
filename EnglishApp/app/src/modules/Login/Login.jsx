0fd48e3aa74055faa52c3f1d929b2a71f351f9fd
sebastiangs23 <sebastiangs2309@gmail.com>
2024-06-15 12:03:38 -0500

    test: Just add a comment

--- EnglishApp/app/src/modules/Login/Login.jsx
+++ EnglishApp/app/src/modules/Login/Login.jsx
@@ -22,7 +22,7 @@ export default function Login({ navigation }) {
   async function getCountries() {
     try {
       const response = await axios.get(
-        "http://192.168.1.10:5000/countries/get-countries-db"
+        "http://192.168.1.10:5000/countries/get-countries-db" //I'm going to change all the endpoints for the deploy
       );
 
       dispatch(setCountries(response.data));
