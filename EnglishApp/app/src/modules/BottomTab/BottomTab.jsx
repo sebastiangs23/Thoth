23d7f6862ec90f0b654d894ad175876d7240d515
sebastiangs23 <sebastiangs2309@gmail.com>
2024-07-03 23:21:42 -0500

    feat(Front): :sparkles: BoottomTab 0%

---
+++ EnglishApp/app/src/modules/BottomTab/BottomTab.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function BottomTab(){
+    return(
+        <View>
+            <Text>Here its where the Icons are gonna be</Text>
+        </View>
+    )
+};
