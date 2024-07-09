529d7331e0ba35f660e8884b4ea91ceccfe4970b
sebastiangs23 <sebastiangs2309@gmail.com>
2024-07-08 20:23:29 -0500

    feat(Front): :sparkles: Icon on the bottomtab

--- EnglishApp/app/src/modules/BottomTab/BottomTab.jsx
+++ EnglishApp/app/src/modules/BottomTab/BottomTab.jsx
@@ -1,13 +1,16 @@
 import { View, Text } from "react-native";
 import { Icon } from "react-native-elements";
 
-export default function BottomTab({ navigation }){
-    return(
-        <View>
-            <View>
-                <Text>Home</Text>
-                <Text>Statistics</Text>
-            </View>
-        </View>
-    )
-};
+export default function BottomTab({ navigation }) {
+  return (
+    <View>
+      <View>
+        <Icon name="fa-house" type="font-awesome" />
+        <Text>Home</Text>
+      </View>
+      <View>
+        <Text>Statistics</Text>
+      </View>
+    </View>
+  );
+}
