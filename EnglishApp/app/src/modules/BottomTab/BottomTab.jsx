8f60b2e09ed425b329ddab49f352b648a6d59576
sebastiangs23 <sebastiangs2309@gmail.com>
2024-07-21 23:52:58 -0500

    docs: just a reminder

--- EnglishApp/app/src/modules/BottomTab/BottomTab.jsx
+++ EnglishApp/app/src/modules/BottomTab/BottomTab.jsx
@@ -1,6 +1,7 @@
 import { View, Text } from "react-native";
 import { Icon } from "react-native-elements";
 
+// I NEED TO START THIS SHIT 
 export default function BottomTab({ navigation }) {
   return (
     <View>
