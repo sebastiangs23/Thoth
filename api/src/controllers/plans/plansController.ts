15ad23c1efe420397c296d8b2594df036de39af5
sebastiangs23 <sebastiangs2309@gmail.com>
2024-12-28 14:13:11 -0500

    perf(CLIENT): :zap: Change the description in the pro and normal plan to look more attractive

--- api/src/controllers/plans/plansController.ts
+++ api/src/controllers/plans/plansController.ts
@@ -6,8 +6,9 @@ import UserPlanHistory from "../../models/plans/users_plan_history_model.js";
 export async function getPlans(req: Request, res: Response){
     try{
         const plans = await Plans.findAll({
-            attributes: ['id', 'name', 'price_usd', 'description', 'icon_name', 'icon_type']
-        });
+            attributes: ['id', 'name', 'price_usd', 'description', 'icon_name', 'icon_type'],
+            order: [['id', 'ASC']]
+        },);
 
         res.json(plans);
 
